5c2e2eb8eafd3374030aedb89783ea58260d9563
JJJ4032002 <rp4888888@gmail.com>
2021-08-15 21:04:41 +0530

    No of ships variable added

--- src/gameBoardFactory.js
+++ src/gameBoardFactory.js
@@ -4,6 +4,7 @@ import { ship } from "./shipFactory";
 
 const methodObj = {
   placeShip: function (ShipLength, position) {
+    this.NoOfShips++;
     let ChildNodes = document.querySelector("#container").children;
     ChildNodes = [...ChildNodes];
     let newShip = ship(ShipLength);
@@ -13,17 +14,55 @@ const methodObj = {
       position,
       ShipCount: 0,
     });
-    console.log(document.querySelector("#container").innerHTML);
+  },
+  receiveAttack: function (position, shipLength) {
+    let index = this.CoordinatesArr.indexOf(position);
+    if (shipLength === undefined) {
+      this.CoordinatesArr.push(position);
+      return "Enemy Missed the attack";
+    } else if (index === -1) {
+      this.CoordinatesArr.push(position);
+      function FindShip(params) {
+        return shipLength === params.ShipLength;
+      }
+      let ShipIndex = this.shipsArr.findIndex(FindShip);
+      let thatShip = this.shipsArr[ShipIndex];
+
+      thatShip.hit(position);
+      let shipCondition = thatShip.isSunk();
+      if (shipCondition === "Oh the ship has been sunk") {
+        this.ShipsSunkArr.push(thatShip);
+      }
+
+      return "The ship has been hit and Coordinates have been noted";
+    } else {
+      return "You have attacked this position";
+    }
+  },
+  AllShipsSunk: function () {
+    if (this.ShipsSunkArr.length === this.NoOfShips) {
+      return "All the ships are sunk";
+    } else {
+      return "All the ships have not been sunk yet";
+    }
   },
 };
 
 function gameBoard(length) {
   let shipsArr = [];
+  let CoordinatesArr = [];
+  let NoOfShips = 0;
+  let ShipsSunkArr = [];
   CreateGrid(length);
   let gameBoardObj = Object.assign(Object.create(methodObj), {
     shipsArr,
+    CoordinatesArr,
+    NoOfShips,
+    ShipsSunkArr,
   });
   return gameBoardObj;
 }
 
-export { gameBoard };
+let newGameBoard = gameBoard(20);
+
+export { gameBoard, newGameBoard };
