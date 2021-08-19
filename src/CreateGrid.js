8bdac20afe4d64df4242e1f2ec65f6dfe216c3b1
JJJ4032002 <rp4888888@gmail.com>
2021-08-19 20:41:14 +0530

    changes in CreateGrid, Added a new parameter and validation

--- src/CreateGrid.js
+++ src/CreateGrid.js
@@ -1,9 +1,18 @@
-function CreateGrid(params) {
+function CreateGrid(params, Board) {
+  let gameDiv = document.querySelector(".BlocksContainer");
+  if (gameDiv === null) {
+    gameDiv = document.createElement("div");
+    gameDiv.setAttribute("class", "BlocksContainer");
+    let OuterContainer = document.querySelector(".OuterContainer");
+    OuterContainer.appendChild(gameDiv);
+  }
   const Container = document.createElement("div");
-  Container.setAttribute("id", "container");
-  document.body.appendChild(Container);
+  Container.setAttribute("class", "container");
+  Container.setAttribute("id", `${Board}`);
+  gameDiv.appendChild(Container);
   for (let i = 0; i < params; i++) {
     const IndDiv = document.createElement("div");
+    IndDiv.classList.add("box-items");
     IndDiv.setAttribute("id", i);
     Container.appendChild(IndDiv);
   }
