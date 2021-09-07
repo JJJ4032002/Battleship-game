d1ce2a761125a30560b75166c039354ef420d6bd
JJJ4032002 <rp4888888@gmail.com>
2021-09-07 18:40:18 +0530

    Added element creator to its individual component

---
+++ src/components/elementCreator.js
@@ -0,0 +1,8 @@
+function elementCreator(type, properties) {
+  let ele = document.createElement(type);
+  for (let prop in properties) {
+    ele[prop] = properties[prop];
+  }
+  return ele;
+}
+export default elementCreator;
