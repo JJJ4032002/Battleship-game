535d043974e5d8a993e995a96055013b6266dc91
JJJ4032002 <rp4888888@gmail.com>
2021-09-08 05:09:12 +0530

    removed additional methods and added them to their individual components

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,8 +1,6 @@
-import { gameBoard } from "./gameBoardFactory";
+import submitAndStart from "./components/submitAndStart";
 
 let gameLoop = (function () {
-  let mainDiv = document.querySelector(".OuterContainer");
-  let ClickButton = null;
   let boards;
 
   function hasClass(elem, className) {
@@ -24,84 +22,6 @@ let gameLoop = (function () {
     },
     false
   );
-
-  function CreateNav() {
-    let nav = elementCreator("nav", { className: "navContainer" });
-    let Heading = document.createElement("h1");
-    Heading.textContent = "Battleship";
-    nav.appendChild(Heading);
-    mainDiv.appendChild(nav);
-  }
-
-  function StartGame() {
-    let GameDiv = elementCreator("div", { id: "gameContainer" });
-    GameDiv.innerHTML = `<label for="Name">Enter your Name</label>
-    <br>
-     <input type="text" id="name" value="">
-     <br>
-     <button class="submitButton">PLAY</button>
-    `;
-    mainDiv.appendChild(GameDiv);
-    ClickButton = document.querySelector(".submitButton");
-  }
-
-  function submitAndStart() {
-    let Input = document.querySelector("#name");
-    let name = Input.value;
-    if (name === "") {
-      alert("Please Enter the name");
-    } else {
-      let gameDiv = document.querySelector("#gameContainer");
-      gameDiv.innerHTML = "";
-      let returnBoards = startMainGame();
-      console.log(returnBoards);
-      return returnBoards;
-    }
-  }
-
-  function startMainGame() {
-    let newBoard1 = gameBoard(100, "Board1");
-    let newBoard2 = gameBoard(100, "Board2");
-    PlaceAllShips(
-      newBoard1,
-      [
-        { length: 1, position: 78 },
-        { length: 2, position: 2 },
-        { length: 3, position: 11 },
-        { length: 4, position: 22 },
-        { length: 5, position: 53 },
-      ],
-      "Board1"
-    );
-    PlaceAllShips(
-      newBoard2,
-      [
-        { length: 1, position: 99 },
-        { length: 2, position: 7 },
-        { length: 3, position: 35 },
-        { length: 4, position: 65 },
-        { length: 5, position: 71 },
-      ],
-      "Board2"
-    );
-    return { newBoard1, newBoard2 };
-  }
-
-  function PlaceAllShips(Board, shipsArr, BoardId) {
-    shipsArr.forEach(function (e) {
-      Board.placeShip(e.length, e.position, BoardId);
-    });
-  }
-
-  function elementCreator(type, properties) {
-    let ele = document.createElement(type);
-    for (let prop in properties) {
-      ele[prop] = properties[prop];
-    }
-    return ele;
-  }
-
-  return { CreateNav, StartGame };
 })();
 
 export default gameLoop;
