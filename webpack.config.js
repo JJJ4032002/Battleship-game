47f4cc12ca2835f197c20cb6490df9d9d5f2fe4f
JJJ4032002 <rp4888888@gmail.com>
2021-09-11 20:42:47 +0530

    Added dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
   entry: "./src/index.js",
   mode: "development",
   devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
