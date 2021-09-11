6a67a4e54e2d00ef5dd1bbb50fbc70496671fd91
JJJ4032002 <rp4888888@gmail.com>
2021-09-11 20:45:32 +0530

    Made checkWhoWon function

--- src/components/checkWhoWon.js
+++ src/components/checkWhoWon.js
@@ -1,3 +1,9 @@
-function checkWhoWon(boards) {}
+function checkWhoWon(boards, position1, shipLength1, position2, shipLength2) {
+  console.log(boards, position1, shipLength1, position2, shipLength2);
+  let stat1 = boards.newBoard1.receiveAttack(position1, shipLength1);
+
+  let stat2 = boards.newBoard2.receiveAttack(position2, shipLength2);
+  console.log(stat1, stat2);
+}
 
 export default checkWhoWon;
