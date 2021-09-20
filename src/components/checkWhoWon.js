5e4265c53dc20ba654f63ec871d810bcd944a319
JJJ4032002 <rp4888888@gmail.com>
2021-09-20 22:14:38 +0530

    Added conditions to determine which board is the winner

--- src/components/checkWhoWon.js
+++ src/components/checkWhoWon.js
@@ -1,9 +1,23 @@
 function checkWhoWon(boards, position1, shipLength1, position2, shipLength2) {
   console.log(boards, position1, shipLength1, position2, shipLength2);
   let stat1 = boards.newBoard1.receiveAttack(position1, shipLength1);
+  let ifAllShipsB1 = boards.newBoard1.AllShipsSunk();
 
   let stat2 = boards.newBoard2.receiveAttack(position2, shipLength2);
-  console.log(stat1, stat2);
+  let ifAllShipsB2 = boards.newBoard2.AllShipsSunk();
+
+  if (ifAllShipsB1 === "All the ships are sunk") {
+    return "Board1 is the winner";
+  } else if (
+    ifAllShipsB2 === "All the ships are sunk" &&
+    ifAllShipsB1 === "All the ships are sunk"
+  ) {
+    return "The game has tied";
+  } else if (ifAllShipsB2 === "All the ships are sunk") {
+    return "Board 2 is the winner";
+  } else {
+    return "No board has one the game yet";
+  }
 }
 
 export default checkWhoWon;
