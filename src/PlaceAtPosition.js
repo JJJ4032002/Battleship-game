3c14dfe09f2c6009c6a1e13587278fdd0bd8d542
JJJ4032002 <rp4888888@gmail.com>
2021-09-22 18:29:12 +0530

    Adds green color to board1 ships

--- src/PlaceAtPosition.js
+++ src/PlaceAtPosition.js
@@ -2,7 +2,10 @@ function PlaceAtPosition(e) {
   let StringNum = this.position.toString();
 
   if (e.id === StringNum && this.ShipCount < this.ShipLength) {
-    e.textContent = "S";
+    if (this.BoardId === "Board1") {
+      e.style["background-color"] = "#6eeb5e";
+    }
+
     e.setAttribute("data-ship", this.ShipLength);
     this.position++;
     this.ShipCount++;
