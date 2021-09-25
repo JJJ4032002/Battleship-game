1614e01a44da573b0972e2509814d118e43f2ba0
JJJ4032002 <rp4888888@gmail.com>
2021-09-25 22:14:13 +0530

    Added color validation to check if ship can be placed or not. Green : success , red : error

--- src/components/ChangeColorBlocks.js
+++ src/components/ChangeColorBlocks.js
@@ -1,13 +1,41 @@
 function ChangeColorBlocks(e, shipCount, arr) {
   let i = 0;
-  console.log(e);
+  let j = 0;
+  let startVal = e.id;
+  let endVal = ChangeNum(e.id);
+  //Calculating if ship can be placed in the row. The total should always be lesser or equal to endVal to be placed.
+  while (j < shipCount - 1) {
+    startVal++;
+    j++;
+  }
 
   while (i < shipCount) {
-    e.style["background-color"] = "#6eeb5e";
+    if (startVal < endVal) {
+      e.style["background-color"] = "#6eeb5e";
+    } else if (startVal === endVal) {
+      e.style["background-color"] = "#6eeb5e";
+    } else {
+      e.style["background-color"] = "#de524e";
+    }
+
     arr.push(e);
-    e = e.nextElementSibling;
+    if (Number(e.id) < 99) {
+      e = e.nextElementSibling;
+    } else {
+      console.log("Do not update");
+    }
 
     i++;
+  }
+  function ChangeNum(num) {
+    num = num.toString();
+    let arr = [...num];
+    num = "";
+    arr[1] = "9";
+    arr.forEach((e) => {
+      num += e;
+    });
+    return Number(num);
   }
 }
 
