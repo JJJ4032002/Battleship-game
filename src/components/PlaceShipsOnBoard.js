8526beab5aacaf47a98b961488f2180e0d8291b3
JJJ4032002 <rp4888888@gmail.com>
2021-09-27 10:21:22 +0530

    Added axis button.

--- src/components/PlaceShipsOnBoard.js
+++ src/components/PlaceShipsOnBoard.js
@@ -1,6 +1,15 @@
 import { gameBoard } from "../gameBoardFactory";
 
 function PlaceShipsOnBoard() {
+  let btnDiv = document.createElement("div");
+  btnDiv.setAttribute("class", "BtnCont");
+  let btn = document.createElement("button");
+  btn.setAttribute("id", "axisBtn");
+  btn.textContent = "X-AXIS";
+  btnDiv.appendChild(btn);
+  let OuterContainer = document.querySelector(".OuterContainer");
+  OuterContainer.appendChild(btnDiv);
+
   let dummyBoard = gameBoard(100, "DummyBoard");
   return dummyBoard;
 }
