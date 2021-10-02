9c9ac93be40eb4164585628a91b7cdc8ade79000
JJJ4032002 <rp4888888@gmail.com>
2021-10-02 23:51:28 +0530

    Added functionlity for preview of vertically aligned ships

--- src/components/ChangeColorBlocks.js
+++ src/components/ChangeColorBlocks.js
@@ -1,15 +1,25 @@
-function ChangeColorBlocks(e, shipCount, arr) {
+function ChangeColorBlocks(e, shipCount, arr, btn) {
   let i = 0;
   let j = 0;
+  let SqrColor = e.id;
   let startVal = e.id;
-  let endVal = ChangeNum(e.id);
+  let endVal = ChangeNum(e.id, btn);
   //Calculating if ship can be placed in the row. The total should always be lesser or equal to endVal to be placed.
-  while (j < shipCount - 1) {
-    startVal++;
-    j++;
+  if (btn) {
+    while (j < shipCount - 1) {
+      startVal++;
+      j++;
+    }
+  } else {
+    while (j < shipCount - 1) {
+      startVal = Number(startVal) + 10;
+      j++;
+    }
   }
 
   while (i < shipCount) {
+    console.log(startVal);
+    console.log(endVal);
     if (startVal < endVal) {
       e.style["background-color"] = "#6eeb5e";
     } else if (startVal === endVal) {
@@ -19,24 +29,48 @@ function ChangeColorBlocks(e, shipCount, arr) {
     }
 
     arr.push(e);
-    if (Number(e.id) < 99) {
+    if (Number(e.id) < 99 && btn) {
       e = e.nextElementSibling;
+    } else if (Number(e.id) < 99 && Number(e.id) < endVal && !btn) {
+      SqrColor = Number(SqrColor) + 10;
+      let nextEl = document.body.querySelector(
+        `[data-box-number = "${SqrColor}"]`
+      );
+      e = nextEl;
     } else {
       console.log("Do not update");
     }
 
     i++;
   }
-  function ChangeNum(num) {
-    num = num.toString();
-    let arr = [...num];
-    num = "";
-    arr[1] = "9";
-    arr.forEach((e) => {
-      num += e;
-    });
-    return Number(num);
+}
+
+function ChangeNum(num, btn) {
+  let numToStr = num.toString();
+
+  let arr = [...numToStr];
+  if (btn) {
+    if (num <= 9) {
+      arr[0] = "9";
+    } else {
+      arr[1] = "9";
+    }
+  } else {
+    if (num <= 9) {
+      let temp = arr[0];
+      arr[0] = "9";
+      arr[1] = temp;
+    } else {
+      arr[0] = "9";
+    }
   }
+
+  num = "";
+
+  arr.forEach((e) => {
+    num += e;
+  });
+  return Number(num);
 }
 
 export default ChangeColorBlocks;
