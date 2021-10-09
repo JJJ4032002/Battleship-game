a938f6eb5e36641de2b250ac20c9d55d1ca6bd0b
JJJ4032002 <rp4888888@gmail.com>
2021-10-09 15:23:13 +0530

    Added condition to place ship in y axis in placeAtPosition.

--- src/PlaceAtPosition.js
+++ src/PlaceAtPosition.js
@@ -2,12 +2,17 @@ function PlaceAtPosition(e) {
   let StringNum = this.position.toString();
 
   if (e.id === StringNum && this.ShipCount < this.ShipLength) {
-    if (this.BoardId === "Board1") {
+    if (this.BoardId !== "Board2") {
       e.style["background-color"] = "#6eeb5e";
     }
 
     e.setAttribute("data-ship", this.ShipLength);
-    this.position++;
+    if (this.axisDecider) {
+      this.position++;
+    } else {
+      this.position = this.position + 10;
+    }
+
     this.ShipCount++;
   }
 }
