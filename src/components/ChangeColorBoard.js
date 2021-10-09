6c363f5bb02d06011df537bb36dae430bad3ace7
JJJ4032002 <rp4888888@gmail.com>
2021-10-09 15:25:48 +0530

    Used the color object instead of using exact values

--- src/components/ChangeColorBoard.js
+++ src/components/ChangeColorBoard.js
@@ -1,14 +1,15 @@
+import Colors from "./Colors";
 function ColorChange(ship1, ship2, ele1, ele2) {
   if (ship1 != null) {
-    ele1.style["background-color"] = "#de524e";
+    ele1.style["background-color"] = Colors.red;
   } else {
-    ele1.style["background-color"] = "#94908f";
+    ele1.style["background-color"] = Colors.gray;
   }
 
   if (ship2 != null) {
-    ele2.style["background-color"] = "#de524e";
+    ele2.style["background-color"] = Colors.red;
   } else {
-    ele2.style["background-color"] = "#94908f";
+    ele2.style["background-color"] = Colors.gray;
   }
 }
 
