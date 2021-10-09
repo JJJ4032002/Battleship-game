60d023e9a3c04be04a69d0e58f9e52b4449860f5
JJJ4032002 <rp4888888@gmail.com>
2021-10-09 15:27:01 +0530

    Added validPlacementShip module

--- src/components/validPlacementShip.js
+++ src/components/validPlacementShip.js
@@ -1,1 +1,21 @@
-function validPlacementShip(id, length) {}
+function validPlacementShip(
+  element,
+  length,
+  ArrToBeCopied,
+  ArrToBeCopiedFrom,
+  board,
+  axisDecider
+) {
+  if (length <= 5) {
+    if (!element.getAttribute("data-ship")) {
+      board.placeShip(length, element.id, "DummyBoard", axisDecider);
+      ArrToBeCopied = [...ArrToBeCopied, ...ArrToBeCopiedFrom];
+      length++;
+    } else {
+      console.log("Ship not placed");
+    }
+  }
+  return { length, ArrToBeCopied };
+}
+
+export default validPlacementShip;
