d765865cbc90eefd5e49759d6e6c42c346dd5ca0
JJJ4032002 <rp4888888@gmail.com>
2021-10-12 19:00:18 +0530

    Added the player name on top while ship preview

--- src/components/PlaceShipsOnBoard.js
+++ src/components/PlaceShipsOnBoard.js
@@ -1,11 +1,14 @@
 import { gameBoard } from "../gameBoardFactory";
 
-function PlaceShipsOnBoard() {
+function PlaceShipsOnBoard(name) {
   let btnDiv = document.createElement("div");
+  let para = document.createElement("p");
+  para.textContent = `Please ${name} place your ships`;
   btnDiv.setAttribute("class", "BtnCont");
   let btn = document.createElement("button");
   btn.setAttribute("id", "axisBtn");
   btn.textContent = "X-AXIS";
+  btnDiv.appendChild(para);
   btnDiv.appendChild(btn);
   let OuterContainer = document.querySelector(".OuterContainer");
   OuterContainer.appendChild(btnDiv);
