79959c1e50bf557e1e08df010e6e2f9eb5014f46
JJJ4032002 <rp4888888@gmail.com>
2021-10-16 17:32:10 +0530

    Added submit button to the placeShipsOnBoard module

--- src/components/PlaceShipsOnBoard.js
+++ src/components/PlaceShipsOnBoard.js
@@ -2,18 +2,23 @@ import { gameBoard } from "../gameBoardFactory";
 
 function PlaceShipsOnBoard(name) {
   let btnDiv = document.createElement("div");
+  let SubBtn = document.createElement("button");
   let para = document.createElement("p");
   para.textContent = `Please ${name} place your ships`;
   btnDiv.setAttribute("class", "BtnCont");
   let btn = document.createElement("button");
   btn.setAttribute("id", "axisBtn");
+  SubBtn.setAttribute("id", "subBtn");
+  SubBtn.textContent = "SUBMIT";
   btn.textContent = "X-AXIS";
   btnDiv.appendChild(para);
   btnDiv.appendChild(btn);
+  btnDiv.appendChild(SubBtn);
   let OuterContainer = document.querySelector(".OuterContainer");
   OuterContainer.appendChild(btnDiv);
 
   let dummyBoard = gameBoard(100, "DummyBoard");
+
   return dummyBoard;
 }
 
