6de4e9bf53168c5ade89ad04615582ebb8a777df
JJJ4032002 <rp4888888@gmail.com>
2021-10-17 04:38:40 +0530

    Removed multiple arguments from checkwhowon function and added other features in its own module

--- src/components/checkWhoWon.js
+++ src/components/checkWhoWon.js
@@ -1,19 +1,12 @@
-function checkWhoWon(boards, position1, shipLength1, position2, shipLength2) {
-  console.log(boards, position1, shipLength1, position2, shipLength2);
-  let stat1 = boards.newBoard1.receiveAttack(position1, shipLength1);
-  let ifAllShipsB1 = boards.newBoard1.AllShipsSunk();
-
-  let stat2 = boards.newBoard2.receiveAttack(position2, shipLength2);
-  let ifAllShipsB2 = boards.newBoard2.AllShipsSunk();
-
-  if (ifAllShipsB1 === "All the ships are sunk") {
+function checkWhoWon(props) {
+  if (props.ifAllShipsB1 === "All the ships are sunk") {
     return "Board1 has lost the game";
   } else if (
-    ifAllShipsB2 === "All the ships are sunk" &&
-    ifAllShipsB1 === "All the ships are sunk"
+    props.ifAllShipsB2 === "All the ships are sunk" &&
+    props.ifAllShipsB1 === "All the ships are sunk"
   ) {
     return "The game has tied";
-  } else if (ifAllShipsB2 === "All the ships are sunk") {
+  } else if (props.ifAllShipsB2 === "All the ships are sunk") {
     return "Board 2 has lost the game";
   } else {
     return "No board has one the game yet";
