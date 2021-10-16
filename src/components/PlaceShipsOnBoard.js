4ac5d3e2fc4028eb71397b0fdfcedf80a694d4fc
JJJ4032002 <rp4888888@gmail.com>
2021-10-17 04:40:07 +0530

    Changed p tag to h1 tag

--- src/components/PlaceShipsOnBoard.js
+++ src/components/PlaceShipsOnBoard.js
@@ -3,15 +3,15 @@ import { gameBoard } from "../gameBoardFactory";
 function PlaceShipsOnBoard(name) {
   let btnDiv = document.createElement("div");
   let SubBtn = document.createElement("button");
-  let para = document.createElement("p");
-  para.textContent = `Please ${name} place your ships`;
+  let heading = document.createElement("h2");
+  heading.textContent = `Please ${name} place your ships`;
   btnDiv.setAttribute("class", "BtnCont");
   let btn = document.createElement("button");
   btn.setAttribute("id", "axisBtn");
   SubBtn.setAttribute("id", "subBtn");
   SubBtn.textContent = "SUBMIT";
   btn.textContent = "X-AXIS";
-  btnDiv.appendChild(para);
+  btnDiv.appendChild(heading);
   btnDiv.appendChild(btn);
   btnDiv.appendChild(SubBtn);
   let OuterContainer = document.querySelector(".OuterContainer");
