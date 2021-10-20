88fe996ada1f403bbb11e43709882c577846463f
JJJ4032002 <rp4888888@gmail.com>
2021-10-20 13:53:17 +0530

    Created restart button

--- src/components/startMainGame.js
+++ src/components/startMainGame.js
@@ -1,5 +1,6 @@
 import PlaceAllShips from "./placeAllShips";
 import { gameBoard } from "../gameBoardFactory";
+import elementCreator from "./elementCreator";
 
 function startMainGame(arr) {
   let newBoard1 = gameBoard(100, "Board1");
@@ -16,6 +17,14 @@ function startMainGame(arr) {
     ],
     "Board2"
   );
+
+  let restBtnDiv = elementCreator("div", { className: "Restart" });
+  let restBtn = elementCreator("button", { id: "RestartBtn" });
+  restBtn.textContent = "Restart";
+  restBtnDiv.appendChild(restBtn);
+
+  document.querySelector(".OuterContainer").appendChild(restBtnDiv);
+
   return { newBoard1, newBoard2 };
 }
 
