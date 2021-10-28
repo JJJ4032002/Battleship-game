f25b81eb6f2ef43214d2c1dcc3c1f6935692f36a
JJJ4032002 <rp4888888@gmail.com>
2021-10-28 14:32:51 +0530

    Modified shipFactory tests

--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -10,23 +10,19 @@ test("Check if ship has been sunk", () => {
 });
 
 test("Hit function test", () => {
-  expect(shipFake.hit(3)).toBe("This position has already been hit");
+  expect(shipFake.hit()).toBe("The ship has been hit");
 });
 
 test("Hit function test", () => {
-  expect(shipFake.hit(2)).toBe("The ship has been hit");
+  expect(shipFake.hit()).toBe("The ship has been hit");
 });
 
 test("Hit function test", () => {
-  expect(shipFake.hit(1)).toBe("The ship has been hit");
+  expect(shipFake.hit()).toBe("The ship has been hit");
 });
 
 test("Hit function test", () => {
-  expect(shipFake.hit(4)).toBe("The ship has been hit");
-});
-
-test("Hit function test", () => {
-  expect(shipFake.hit(5)).toBe("The ship has been hit");
+  expect(shipFake.hit()).toBe("The ship has been hit");
 });
 
 test("Check if ship has been sunk", () => {
