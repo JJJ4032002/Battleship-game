41ebdd33a6cd5f09d4df64726cb966857ec44ef3
JJJ4032002 <rp4888888@gmail.com>
2021-10-31 04:56:46 +0530

    Modified previewShips modulw

--- src/components/PreviewShipsOnHover.js
+++ src/components/PreviewShipsOnHover.js
@@ -2,28 +2,23 @@ import Colors from "./Colors";
 function PreviewShipsOnHover(e, shipCount, arr, arrRed, btn) {
   let i = 0;
   let j = 0;
-  let SqrColor = e.id;
-  let startVal = e.id;
-  let endVal = ChangeNum(e.id, btn);
+
+  let row = e.getAttribute("data-row");
+  let column = e.getAttribute("data-column");
+
   //Calculating if ship can be placed in the row. The total should always be lesser or equal to endVal to be placed.
-  if (btn) {
-    while (j < shipCount - 1) {
-      startVal++;
-      j++;
+
+  while (j < shipCount - 1) {
+    if (btn) {
+      column++;
+    } else {
+      row++;
     }
-  } else {
-    while (j < shipCount - 1) {
-      startVal = Number(startVal) + 10;
-      j++;
-    }
+    j++;
   }
 
   while (i < shipCount) {
-    console.log(startVal);
-    console.log(endVal);
-    if (Number(startVal) < endVal) {
-      e.style["background-color"] = Colors.green;
-    } else if (Number(startVal) === endVal) {
+    if ((btn && column <= 9) || (!btn && row <= 9)) {
       e.style["background-color"] = Colors.green;
     } else {
       e.style["background-color"] = Colors.red;
@@ -31,12 +26,15 @@ function PreviewShipsOnHover(e, shipCount, arr, arrRed, btn) {
     }
 
     arr.push(e);
-    if (Number(e.id) < 99 && btn) {
+    if (btn && e.getAttribute("data-column") < 9) {
       e = e.nextElementSibling;
-    } else if (Number(e.id) < 99 && Number(e.id) < endVal && !btn) {
-      SqrColor = Number(SqrColor) + 10;
+    } else if (!btn && e.getAttribute("data-row") < 9) {
+      let rowColor = e.getAttribute("data-row");
+      let columnColor = e.getAttribute("data-column");
+
+      rowColor++;
       let nextEl = document.body.querySelector(
-        `[data-box-number = "${SqrColor}"]`
+        `[data-Coordinates = "${rowColor}${columnColor}"]`
       );
       e = nextEl;
     } else {
@@ -45,34 +43,6 @@ function PreviewShipsOnHover(e, shipCount, arr, arrRed, btn) {
 
     i++;
   }
-}
-
-function ChangeNum(num, btn) {
-  let numToStr = num.toString();
-
-  let arrChangeNum = [...numToStr];
-  if (btn) {
-    if (num <= 9) {
-      arrChangeNum[0] = "9";
-    } else {
-      arrChangeNum[1] = "9";
-    }
-  } else {
-    if (num <= 9) {
-      let temp = arrChangeNum[0];
-      arrChangeNum[0] = "9";
-      arrChangeNum[1] = temp;
-    } else {
-      arrChangeNum[0] = "9";
-    }
-  }
-
-  num = "";
-
-  arrChangeNum.forEach((e) => {
-    num += e;
-  });
-  return Number(num);
 }
 
 export default PreviewShipsOnHover;
