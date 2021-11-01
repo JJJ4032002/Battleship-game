fd4fc23105bd7465536ec4d473287f6328b2f3e0
JJJ4032002 <rp4888888@gmail.com>
2021-11-01 11:51:24 +0530

    Modified validPlacementSHip so it only uses dom to display the changes made.

--- src/components/validPlacementShip.js
+++ src/components/validPlacementShip.js
@@ -1,15 +1,14 @@
+import PlaceAtPosition from "../PlaceAtPosition";
 function validPlacementShip({
   element,
   length,
   ArrToBeCopied,
-  ArrToBeCopiedFrom,
-  arrRed,
-  shipCoordinatesArr,
+
   board,
   axisDecider,
 }) {
-  let row = document.getAttribute("data-row");
-  let column = document.getAttribute("data-column");
+  let row = element.getAttribute("data-row");
+  let column = element.getAttribute("data-column");
   let ifShipCanBePlaced = board.checkValidShipPlacement(
     row,
     column,
@@ -17,35 +16,17 @@ function validPlacementShip({
     axisDecider
   );
   if (ifShipCanBePlaced) {
-    board.placeShip(row, column, shipLength, axisDecider);
+    board.placeShip(row, column, length, axisDecider);
+    ArrToBeCopied = PlaceAtPosition(
+      row,
+      column,
+      length,
+      axisDecider,
+      ArrToBeCopied
+    );
+    length++;
   }
-  return { length, ArrToBeCopied, shipCoordinatesArr };
-}
-
-function validateTheArr(element, axisDecider, ArrToBeCopied, arrRed, length) {
-  let BlockCount = 0;
-  let arrToValidate = [];
-  let tempEle = element;
-  let tempId = element.id;
-  while (BlockCount < length) {
-    arrToValidate.push(tempEle);
-    if (axisDecider) {
-      tempId++;
-    } else {
-      tempId = Number(tempId) + 10;
-    }
-    tempEle = document.querySelector(`[data-box = "DummyBoard${tempId}"]`);
-    BlockCount++;
-  }
-  const ValidatedArr = arrToValidate.filter((e) => {
-    return ArrToBeCopied.includes(e);
-  });
-
-  const ValidatedArrRed = arrToValidate.filter((e) => {
-    return arrRed.includes(e);
-  });
-
-  return { ValidatedArr, ValidatedArrRed };
+  return { length, ArrToBeCopied };
 }
 
 export default validPlacementShip;
