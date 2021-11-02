a8aeb34b069182ef0bb13f7d93b56a715806eebd
JJJ4032002 <rp4888888@gmail.com>
2021-11-02 14:33:08 +0530

    Updated change color of board method.

--- src/components/ChangeColorBoard.js
+++ src/components/ChangeColorBoard.js
@@ -1,15 +1,24 @@
 import Colors from "./Colors";
-function ColorChange(ship1, ship2, ele1, ele2) {
-  if (ship1 != null) {
-    ele1.style["background-color"] = Colors.red;
+function ColorChange(
+  Board2Cond,
+  Board1Cond,
+  CoordinatesOneArr,
+  CoordinatesTwoArr
+) {
+  let [row1, column1] = CoordinatesOneArr;
+  let [row2, column2] = CoordinatesTwoArr;
+  let ele2 = document.querySelector(`#Board2${row2}${column2}`);
+  let ele1 = document.querySelector(`#Board1${row1}${column1}`);
+  if (Board2Cond === "oops you missed the ship") {
+    ele2.style["background-color"] = Colors.gray;
   } else {
-    ele1.style["background-color"] = Colors.gray;
+    ele2.style["background-color"] = Colors.red;
   }
 
-  if (ship2 != null) {
-    ele2.style["background-color"] = Colors.red;
+  if (Board1Cond === "oops you missed the ship") {
+    ele1.style["background-color"] = Colors.gray;
   } else {
-    ele2.style["background-color"] = Colors.gray;
+    ele1.style["background-color"] = Colors.red;
   }
 }
 
