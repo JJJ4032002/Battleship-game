b1f0e25a7b604d8e523d0a7e131af39fd66a65be
JJJ4032002 <rp4888888@gmail.com>
2022-12-25 22:50:56 +0530

    Move styles to src

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,24 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.mp4$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
